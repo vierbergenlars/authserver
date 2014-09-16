9a189b8a8e535d0c3f3e0fdd6eaf93b1422d052d
Lars Vierbergen <vierbergenlars@gmail.com>
2014-09-16 14:21:39 +0200

    Add forms

---
+++ src/App/Form/OAuth/ClientType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form\OAuth;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ClientType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('allowedGrantTypes', 'choice', array(
+                'choices' => array(
+                    'authorization_code' => 'authorization_code',
+                ),
+                'multiple'=>true,
+                'expanded' => true,
+            ))
+            ->add('redirectUris', 'collection', array(
+                'type' => 'text',
+                'allow_add' => true,
+                'allow_delete'=>true
+            ))
+            ->add('submit', 'submit')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'App\Entity\OAuth\Client'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'app_oauth_client';
+    }
+}
---
+++ src/App/Form/UserType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password')
+            ->add('email')
+            ->add('roles', 'choice', array(
+                'choices' => array(
+                    'ROLE_USER' => 'User',
+                    'ROLE_ADMIN' => 'Admin',
+                    'ROLE_SUPER_ADMIN' => 'Super admin',
+                ),
+                'multiple'=>true,
+                'expanded' => true,
+            ))
+            ->add('enabled')
+            ->add('submit', 'submit')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'App\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'app_user';
+    }
+}
