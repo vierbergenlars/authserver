e2e04bb659ca6b201d5a9362e47dd3d94c88803f
Lars Vierbergen <vierbergenlars@gmail.com>
2014-09-26 12:11:58 +0200

    Add a way for the user to delete authorized apps; add flash messages support

--- src/User/Controller/ProfileController.php
+++ src/User/Controller/ProfileController.php
@@ -4,6 +4,9 @@ namespace User\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use User\Form\DeleteAuthorizedAppType;
+use App\Entity\User;
 
 class ProfileController extends Controller
 {
@@ -13,5 +16,40 @@ class ProfileController extends Controller
     public function indexAction()
     {
         return $this->getUser();
+    }
+
+    /**
+     * @Template
+     * Internal action, not exposed in a route
+     */
+    public function removeAuthorizedAppAction($appId)
+    {
+        return $this->createForm(new DeleteAuthorizedAppType(), array('id'=>$appId));
+    }
+
+    public function deleteAuthorizedAppAction(Request $request)
+    {
+        $form = $this->createForm(new DeleteAuthorizedAppType());
+
+        $form->handleRequest($request);
+
+        if($form->isValid()) {
+            $appId = $form->get('id')->getData();
+            $client = $this->getDoctrine()
+                ->getRepository('AppBundle:OAuth\Client')
+                ->find($appId);
+
+            if($client && ($user = $this->getUser()) && $user instanceof User) {
+                $user->removeAuthorizedApplication($client);
+                $this->getDoctrine()->getRepository('AppBundle:User')->update($user);
+                $this->get('braincrafted_bootstrap.flash')->success('Authorized application has been removed');
+
+                return $this->redirect($this->generateUrl('user_profile'));
+            }
+        }
+
+        $this->get('braincrafted_bootstrap.flash')->error('Error removing authorized application');
+
+        return $this->redirect($this->generateUrl('user_profile'));
     }
 }
---
+++ src/User/Form/DeleteAuthorizedAppType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace User\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
+
+class DeleteAuthorizedAppType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->setMethod('DELETE')
+            ->add('id', 'hidden')
+            ->add('submit', 'submit', array(
+                'label'=>'Delete',
+                'attr' => array(
+                    'class' => 'btn-danger btn-sm pull-right',
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'usr_auth_app_delete';
+    }
+}
