5f7b9e564497b5aa2892ddeb5a1ef467eaa5988c
Lars Vierbergen <vierbergenlars@gmail.com>
2014-09-28 11:12:05 +0200

    Add (UN)LINK support to admin group

--- src/Admin/Controller/GroupController.php
+++ src/Admin/Controller/GroupController.php
@@ -2,7 +2,44 @@
 
 namespace Admin\Controller;
 
+use Admin\Controller\Traits\Routes\LinkUnlinkTrait;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use App\Entity\Group;
+
 class GroupController extends DefaultController
 {
+    use LinkUnlinkTrait;
+
+    protected function handleLink($type, $parent, $link)
+    {
+        switch($type) {
+            case 'group':
+                $group = $link->getData();
+                if(!$group instanceof Group) {
+                    throw new BadRequestHttpException('Subresource of wrong type (expected: group)');
+                }
+                if(!$parent->getGroups()->contains($group)) {
+                    $parent->addGroup($group);
+                }
+                break;
+            default:
+                throw new BadRequestHttpException('Invalid relationship (allowed: group)');
+        }
+    }
+
+    protected function handleUnlink($type, $parent, $link)
+    {
+        switch($type) {
+            case 'group':
+                $group = $link->getData();
+                if(!$group instanceof Group) {
+                    throw new BadRequestHttpException('Subresource of wrong type (expected: group)');
+                }
+                $parent->removeGroup($group);
+                break;
+            default:
+                throw new BadRequestHttpException('Invalid relationship (allowed: group)');
+        }
+    }
 
 }
