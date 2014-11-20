507d3179a3bb540147220f5cd553366a8afa2e05
Lars Vierbergen <vierbergenlars@gmail.com>
2014-11-20 10:51:54 +0100

    Add setting as primary email and resending confirmations to user profile

--- src/User/Controller/ProfileController.php
+++ src/User/Controller/ProfileController.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Request;
 use User\Form\DeleteAuthorizedAppType;
 use App\Entity\User;
 use User\Form\ChangePasswordType;
+use User\Form\EditEmailAddressType;
+use App\Entity\EmailAddress;
 
 class ProfileController extends Controller
 {
@@ -50,6 +52,75 @@ class ProfileController extends Controller
         }
 
         $this->get('braincrafted_bootstrap.flash')->error('Error removing authorized application');
+
+        return $this->redirectToProfile();
+    }
+
+    /**
+     * @Template
+     * Internal action, not exposed in a route
+     */
+    public function editEmailAddressesAction(EmailAddress $addr)
+    {
+        return array('form'=>$this->createForm(new EditEmailAddressType(), array('id'=>$addr->getId()))->createView(), 'data'=>$addr);
+    }
+
+    public function putEmailAddressesAction(Request $request)
+    {
+        $flash = $this->get('braincrafted_bootstrap.flash');
+
+        $form  = $this->createForm(new EditEmailAddressType());
+
+        $form->handleRequest($request);
+
+        $em = $this->getDoctrine()->getManagerForClass('AppBundle:EmailAddress');
+
+        if($form->isValid()) {
+            $id = $form->get('id')->getData();
+            $addresses = $this->getUser()->getEmailAddresses();
+            $addr = $addresses->filter(function(EmailAddress $e) use($id) {
+                return $e->getId() == $id;
+            })->first();
+
+            if(!$addr) {
+                throw $this->createNotFoundException();
+            }
+
+            switch($form->getClickedButton()->getName()) {
+                case 'sendConfirmation':
+                    if(!$addr->isVerified()) {
+                        $addr->setVerified(false);
+                        $this->get('app.mailer')
+                            ->sendMessage(
+                                'AppBundle:Mail:verify_email.mail.twig',
+                                array('data'=>$addr),
+                                $addr->getEmail()
+                            );
+
+                        $flash->success('A new confirmation email has been sent');
+                    }
+                    break;
+                case 'setPrimary':
+                    if($addr->isVerified()) {
+                        $addresses->map(function(EmailAddress $e) {
+                            if($e->isPrimary())
+                                $e->setPrimary(false);
+                        });
+
+                        $addr->setPrimary(true);
+                        $flash->success('Primary email address updated');
+                    } else {
+                        $flash->error('Please verify this email address before setting it as primary email address');
+                    }
+                    break;
+                default:
+                    // Should never happen
+                    $flash->error('Internal error: Unknown button pressed');
+            }
+            $em->flush($addresses->toArray());
+        } else {
+            $flash->error('Error modifying email address');
+        }
 
         return $this->redirectToProfile();
     }
