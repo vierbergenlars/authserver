515e139ff5c0871fc22a68eb8b259b96253c4235
Lars Vierbergen <vierbergenlars@gmail.com>
2015-02-06 16:07:18 +0100

    Add editing of properties to user profile

--- src/User/Controller/ProfileController.php
+++ src/User/Controller/ProfileController.php
@@ -2,15 +2,18 @@
 
 namespace User\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\Request;
-use User\Form\DeleteAuthorizedAppType;
+use App\Entity\EmailAddress;
+use App\Entity\Property;
 use App\Entity\User;
+use App\Entity\UserProperty;
+use Braincrafted\Bundle\BootstrapBundle\Session\FlashMessage;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use User\Form\AddPasswordType;
 use User\Form\ChangePasswordType;
+use User\Form\DeleteAuthorizedAppType;
 use User\Form\EditEmailAddressType;
-use App\Entity\EmailAddress;
 use User\Form\EmailAddressType;
 
 class ProfileController extends Controller
@@ -190,6 +193,46 @@ class ProfileController extends Controller
 
         return $this->redirectToProfile();
     }
+    
+    /**
+     * @Template
+     */
+    public function editPropertyAction(UserProperty $property) {
+        return array(
+            'form' => $this->createForm(new \User\Form\EditUserPropertyType(), $property)
+                        ->createView(),
+            'data' => $property
+        );
+    }
+    
+    public function putPropertyAction(Property $property, Request $request) {
+        if(!$property->isUserEditable())
+            throw $this->createNotFoundException();
+
+        $em = $this->getDoctrine()
+                ->getManagerForClass('AppBundle:UserProperty');
+        $userProperty = $em->getRepository('AppBundle:UserProperty')
+                ->findOneBy(array(
+                    'user' => $this->getUser(),
+                    'property' => $property
+                ));
+
+        $form = $this->createForm(new \User\Form\EditUserPropertyType(), $userProperty);
+        $form->handleRequest($request);
+
+        if($form->isValid()) {
+            $em->persist($userProperty);
+            $em->flush($userProperty);
+            $this->getFlash()->success('Information updated successfully.');
+        } else {
+            if($property->isRequired() && !$userProperty->getData()) {
+                $this->getFlash()->error('This field should not be left blank. Please try again.');
+            } else {
+                $this->getFlash()->error('Updating information failed.');
+            }
+        }
+        return $this->redirectToProfile();
+    }
 
     /**
      * @Template
@@ -234,5 +277,13 @@ class ProfileController extends Controller
     private function redirectToProfile()
     {
         return $this->redirect($this->generateUrl('user_profile'));
+    }
+    
+    /**
+     * 
+     * @return FlashMessage
+     */
+    private function getFlash() {
+        return $this->get('braincrafted_bootstrap.flash');
     }
 }
