bcdbbd9e1b20373e52195e8ddd9a03687445b8f0
Lars Vierbergen <vierbergenlars@gmail.com>
2015-02-08 14:31:13 +0100

    Add extra batch actions to user, oauth client, user & user property admin

--- src/Admin/Controller/OAuthClientController.php
+++ src/Admin/Controller/OAuthClientController.php
@@ -4,5 +4,10 @@ namespace Admin\Controller;
 
 class OAuthClientController extends DefaultController
 {
-
+    protected function getBatchActions() {
+        $actions = parent::getBatchActions();
+        $actions['Pre approved']['PATCH_preApproved_true'] = 'Enable';
+        $actions['Pre approved']['PATCH_preApproved_false'] = 'Disable';
+        return $actions;
+    }
 }
--- src/Admin/Controller/UserPropertyController.php
+++ src/Admin/Controller/UserPropertyController.php
@@ -4,5 +4,12 @@ namespace Admin\Controller;
 
 class UserPropertyController extends DefaultController
 {
-
+    protected function getBatchActions() {
+        $actions = parent::getBatchActions();
+        $actions['Editable by user']['PATCH_userEditable_true'] = 'Enable';
+        $actions['Editable by user']['PATCH_userEditable_false'] = 'Disable';
+        $actions['Required']['PATCH_required_true'] = 'Enable';
+        $actions['Required']['PATCH_required_false'] = 'Disable';
+        return $actions;
+    }
 }
