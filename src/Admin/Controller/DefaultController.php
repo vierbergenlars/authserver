efc070807ac5f2d5d3743ac24b0d9a9c923c391f
Lars Vierbergen <vierbergenlars@gmail.com>
2015-02-08 14:05:50 +0100

    Add batch actions on a list of items

--- src/Admin/Controller/DefaultController.php
+++ src/Admin/Controller/DefaultController.php
@@ -2,33 +2,35 @@
 
 namespace Admin\Controller;
 
+use Admin\Form\BatchType;
+use App\Search\SearchException;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\NoRoute;
+use FOS\RestBundle\Controller\Annotations\View as AView;
+use JMS\Serializer\SerializationContext;
+use Knp\Component\Pager\Paginator;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use FOS\RestBundle\Controller\Annotations\Post;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use vierbergenlars\Bundle\RadRestBundle\Controller\ControllerServiceController;
+use vierbergenlars\Bundle\RadRestBundle\Controller\RadRestControllerInterface;
+use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\DefaultsTrait;
 use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Pagination\KnpPaginationTrait;
+use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Routes\CreateTrait;
+use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Routes\DeleteTrait;
+use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Routes\EditTrait;
+use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Routes\ListTrait;
+use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Routes\PatchTrait;
+use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Routes\ViewTrait;
 use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Routing\ClassResourceRoutingTrait;
 use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Serialization\ClassResourceSerializationGroupsTrait;
-use Knp\Component\Pager\Paginator;
-use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\DefaultsTrait;
-use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Routes\ListTrait;
-use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Routes\ViewTrait;
 use vierbergenlars\Bundle\RadRestBundle\Manager\FrontendManager;
-use vierbergenlars\Bundle\RadRestBundle\Controller\RadRestControllerInterface;
-use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Routes\CreateTrait;
-use FOS\RestBundle\Controller\Annotations\View as AView;
-use FOS\RestBundle\Controller\Annotations\Get;
-use Symfony\Component\HttpFoundation\Request;
-use Nelmio\ApiDocBundle\Annotation\ApiDoc;
-use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Routes\EditTrait;
-use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Routes\DeleteTrait;
-use vierbergenlars\Bundle\RadRestBundle\View\View;
-use vierbergenlars\Bundle\RadRestBundle\Twig\ControllerVariables;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use App\Search\SearchException;
-use JMS\Serializer\SerializationContext;
-use FOS\RestBundle\Controller\Annotations\NoRoute;
-use vierbergenlars\Bundle\RadRestBundle\Controller\Traits\Routes\PatchTrait;
 use vierbergenlars\Bundle\RadRestBundle\Manager\ResourceManagerInterface;
-use Symfony\Component\Form\FormTypeInterface;
-use Symfony\Component\Form\FormFactoryInterface;
+use vierbergenlars\Bundle\RadRestBundle\Twig\ControllerVariables;
+use vierbergenlars\Bundle\RadRestBundle\View\View;
 
 class DefaultController extends ControllerServiceController
 {
@@ -79,6 +81,56 @@ class DefaultController extends ControllerServiceController
             return $this->_LT_cgetAction($request);
         }
     }
+    
+    /**
+     * @Post
+     */
+    public function batchAction(Request $request)
+    {
+        $form = $this->getFormFactory()->create(new BatchType($this->getBatchActions()));
+        $form->handleRequest($request);
+        if($form->isValid()) {
+            $subjects = $form->get('subjects')->getData();
+            $this->handleBatch($form->get('action')->getData(), $subjects);
+        }
+        return $this->redirectTo('cget', $request->query->get('_params', array()));
+    }
+
+    protected function handleBatch($action, $subjects) {
+        if($action === 'DELETE') {
+            foreach($subjects as $id => $checked) {
+                if($checked) {
+                    $this->getResourceManager()->delete($this->getResourceManager()->find($id));
+                }
+            }
+        } else if(substr($action, 0, 6) === 'PATCH_') {
+            list($patch, $property, $value) = explode('_', $action);
+            foreach($subjects as $id => $checked) {
+                if($checked) {
+                    $group = $this->getResourceManager()->find($id);
+                    $method = 'set'.ucfirst($property);
+                    $group->$method($value === 'false'?false:$value);
+                    $this->getResourceManager()->update($group);
+                }
+            }
+        }
+    }
+    
+    protected function getBatchActions() {
+        return array(
+            'DELETE' => 'Delete',
+        );
+    }
+    
+    /**
+     * @AView
+     * @ApiDoc(resource=true)
+     * @Get
+     */
+    public function getAction($id)
+    {
+        return parent::getAction($id);
+    }
 
     /**
      * @AView
@@ -115,6 +167,7 @@ class DefaultController extends ControllerServiceController
     {
         $view->setExtraData(array(
             'controller' => new ControllerVariables($this),
+            'batch_form' => $this->getFormFactory()->create(new BatchType($this->getBatchActions()))->createView(),
         ));
         $view->getSerializationContext()->enableMaxDepthChecks();
         return $view;
