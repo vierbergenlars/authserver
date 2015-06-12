5ea51229f555e6c34b5c2a3985db3bf2d41362e5
Lars Vierbergen <vierbergenlars@gmail.com>
2015-06-12 15:17:54 +0200

    Also audit log email addresses

--- src/App/Entity/EmailAddress.php
+++ src/App/Entity/EmailAddress.php
@@ -4,9 +4,11 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\OAuthServerBundle\Util\Random;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity
+ * @Gedmo\Loggable
  */
 class EmailAddress implements \Serializable
 {
@@ -19,11 +21,13 @@ class EmailAddress implements \Serializable
 
     /**
      * @ORM\Column(type="string", unique=true)
+     * @Gedmo\Versioned
      */
     private $email;
 
     /**
      * @ORM\Column(type="boolean")
+     * @Gedmo\Versioned
      */
     private $verified = false;
 
@@ -34,12 +38,14 @@ class EmailAddress implements \Serializable
 
     /**
      * @ORM\Column(name="primary_mail", type="boolean")
+     * @Gedmo\Versioned
      */
     private $primary = false;
 
     /**
      * @ORM\ManyToOne(targetEntity="User", inversedBy="emailAddresses")
      * @ORM\JoinColumn(nullable=false)
+     * @Gedmo\Versioned
      * @var User
      */
     private $user;
