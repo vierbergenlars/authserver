36f913d5946595ae16ee5646f6efea0165e3b973
Lars Vierbergen <vierbergenlars@gmail.com>
2015-06-16 21:19:53 +0200

    Add simple deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e # Quit the script on error
+php app/console app:maintenance --env=prod
+composer install --no-dev --no-plugins --no-scripts --optimize-autoloader
+php app/console cache:clear --env=prod
+php app/console assets:install --env=prod
+php app/console assetic:dump --env=prod
+php app/console braincrafted:bootstrap:install --env=prod
+php app/console doctrine:migrations:migrate --env=prod
+php app/console app:maintenance -d --env=prod
