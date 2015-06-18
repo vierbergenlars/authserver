a323d5350d6b9dfaa54315d4338204373d4239e1
Lars Vierbergen <vierbergenlars@gmail.com>
2015-06-18 20:19:47 +0200

    Add npm install to deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 set -e # Quit the script on error
 php app/console app:maintenance --env=prod
 composer install --no-dev --no-plugins --no-scripts --optimize-autoloader
+npm install
 php app/console cache:clear --env=prod
 php app/console assets:install --env=prod
 php app/console assetic:dump --env=prod
