9780711fe0cfe072985a67aaf328d2168cc615db
Lars Vierbergen <vierbergenlars@gmail.com>
2015-06-18 23:52:36 +0200

    [deploy] Only execute migrations when there are available

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,7 @@ php app/console cache:clear --env=prod
 php app/console assets:install --env=prod
 php app/console assetic:dump --env=prod
 php app/console braincrafted:bootstrap:install --env=prod
+# Only execute migrations when there are new migrations available.
+php app/console doctrine:migrations:status --env=prod | grep "New Migrations:" | cut -d: -f2 |grep "^ *0" > /dev/null || \
 php app/console doctrine:migrations:migrate --env=prod
 php app/console app:maintenance -d --env=prod
