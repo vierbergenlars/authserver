358e4122088aa0f2edf544e418c664696627924e
Lars Vierbergen <vierbergenlars@gmail.com>
2015-11-15 20:46:55 +0100

    Refactor OAuthClientController to use CRUDController

--- src/App/Entity/OAuth/Client.php
+++ src/App/Entity/OAuth/Client.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
- * @ORM\Entity(repositoryClass="ClientRepository")
+ * @ORM\Entity
  * @Gedmo\Loggable
  */
 class Client extends BaseClient
