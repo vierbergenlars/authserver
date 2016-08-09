726594b726b23d047dc842c9852328d17dc97ab3
Lars Vierbergen <vierbergenlars@gmail.com>
2016-08-09 17:19:01 +0200

    Fix clic redeploy script

--- .clic-scripts/redeploy.sh
+++ .clic-scripts/redeploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 export SYMFONY_ENV=$($CLIC application:variable:get "$CLIC_APPNAME" app/environment)
-source deploy.inc.sh
+source .clic-scripts/deploy.inc.sh
 printf "\x1b[30;42mRe-deploy finished\x1b[0m \x1b[30:46m$(git rev-parse --short HEAD)\x1b[0m\n"
