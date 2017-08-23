f8e86a5faa2d5077cb8b861c61b711db4bbf8eac
Lars Vierbergen <vierbergenlars@gmail.com>
2017-08-23 21:30:24 +0200

    Start using plugin EventDispatcher to separate security.firewall config into bundles

--- src/App/AppBundle.php
+++ src/App/AppBundle.php
@@ -19,9 +19,78 @@
 
 namespace App;
 
+use App\Plugin\Event\ContainerConfigEvent;
+use App\Plugin\PluginEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
-class AppBundle extends Bundle
+class AppBundle extends Bundle implements EventSubscriberInterface
 {
 
+    public static function getSubscribedEvents()
+    {
+        return [
+            PluginEvents::CONTAINER_CONFIG => 'loadFirewallConfig',
+        ];
+    }
+
+    public function loadFirewallConfig(ContainerConfigEvent $event)
+    {
+        $config = $event->getConfig();
+        $pa = PropertyAccess::createPropertyAccessor();
+        $firewall = $pa->getValue($config, '[security][firewall]');
+        if(!$firewall)
+            $firewall = [];
+
+        $firewallOrder = array_keys($firewall);
+
+        if($event->getKernel()->getEnvironment() === 'dev') {
+            array_unshift($firewallOrder, 'dev');
+            $firewall['dev'] = [
+                'name' => 'dev',
+                'pattern'  => '^/(_(profiler|wdt)|css|images|js)/',
+                'security' => false,
+            ];
+        }
+
+        array_push($firewallOrder, 'oauth_token', 'api', 'public');
+
+        $firewall['oauth_token'] =[
+            'name' => 'oauth_token',
+            'pattern' => '^/oauth/v2/token',
+            'security' => false
+        ];
+        $firewall['api'] = [
+            'name' => 'api',
+            'pattern' => '^/api',
+            'http_basic' => null,
+            'fos_oauth' => true,
+            'stateless' => true,
+        ];
+        $firewall['public'] = [
+            'name' => 'public',
+            'pattern' => '^/',
+            'form_login' => [
+                'login_path' => 'app_login',
+                'check_path' => 'app_login_check',
+            ],
+            'simple_preauth' => [
+                'authenticator' => 'app.admin.security.apikey_authenticator',
+            ],
+            'logout' => [
+                'handlers' => ['app.security.logout_handler'],
+            ],
+            'anonymous' => null,
+            'switch_user' => true
+        ];
+
+        uksort($firewall, function($a, $b) use($firewallOrder) {
+            return array_search($a, $firewallOrder, true) - array_search($b, $firewallOrder, true);
+        });
+
+        $pa->setValue($config, '[security][firewall]', $firewall);
+
+        $event->setConfig($config);
+    }
 }
--- src/AuthRequestBundle/AuthRequestBundle.php
+++ src/AuthRequestBundle/AuthRequestBundle.php
@@ -2,8 +2,44 @@
 
 namespace AuthRequestBundle;
 
+use App\Plugin\Event\ContainerConfigEvent;
+use App\Plugin\PluginEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
-class AuthRequestBundle extends Bundle
+class AuthRequestBundle extends Bundle implements EventSubscriberInterface
 {
+    public static function getSubscribedEvents()
+    {
+        return [PluginEvents::CONTAINER_CONFIG => ['addFirewallConfig', -1]];
+    }
+
+    public function addFirewallConfig(ContainerConfigEvent $event)
+    {
+        $config = $event->getConfig();
+        $pa = PropertyAccess::createPropertyAccessor();
+        $firewall = $pa->getValue($config, '[security][firewall]');
+        if(!$firewall)
+            $firewall = [];
+
+        $firewallOrder = array_keys($firewall);
+
+        $firewall['auth_request_basic_api'] = [
+            'name' => 'auth_request_basic_api',
+            'pattern' => '^/api/auth_request/basic',
+            'http_basic' => null,
+            'stateless' => true,
+        ];
+
+        array_unshift($firewallOrder, 'auth_request_basic_api');
+
+        uksort($firewall, function($a, $b) use($firewallOrder) {
+            return array_search($a, $firewallOrder, true) - array_search($b, $firewallOrder, true);
+        });
+
+        $pa->setValue($config, '[security][firewall]', $firewall);
+
+        $event->setConfig($config);
+    }
 }
