31adbf24d94eac0a3f9b676992fab72cadee7233
Lars Vierbergen <vierbergenlars@gmail.com>
2018-05-12 17:09:46 +0200

    Place the chained userchecker in an event subscriber as well, so relative order can be changed

--- src/App/Security/User/UserChecker.php
+++ src/App/Security/User/UserChecker.php
@@ -6,8 +6,9 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use App\AppEvents;
 use App\Event\UserCheckerEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class UserChecker implements UserCheckerInterface
+class UserChecker implements UserCheckerInterface, EventSubscriberInterface
 {
 
     /**
@@ -30,13 +31,29 @@ class UserChecker implements UserCheckerInterface
 
     public function checkPreAuth(UserInterface $user)
     {
-        $this->chainedChecker->checkPreAuth($user);
         $this->eventDispatcher->dispatch(AppEvents::SECURITY_USER_CHECK_PRE, new UserCheckerEvent($user));
     }
 
     public function checkPostAuth(UserInterface $user)
     {
-        $this->chainedChecker->checkPostAuth($user);
         $this->eventDispatcher->dispatch(AppEvents::SECURITY_USER_CHECK_POST, new UserCheckerEvent($user));
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            AppEvents::SECURITY_USER_CHECK_PRE => 'onUserCheckPre',
+            AppEvents::SECURITY_USER_CHECK_POST => 'onUserCheckPost'
+        ];
+    }
+
+    public function onUserCheckPre(UserCheckerEvent $event)
+    {
+        $this->chainedChecker->checkPreAuth($event->getUser());
+    }
+
+    public function onUserCheckPost(UserCheckerEvent $event)
+    {
+        $this->chainedChecker->checkPostAuth($event->getUser());
     }
 }
