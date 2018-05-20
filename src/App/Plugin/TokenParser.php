621a466dbf98fbf1fd9ea41a49038f07116a11ef
Lars Vierbergen <vierbergenlars@gmail.com>
2018-05-20 14:44:27 +0200

    Handle classname references (TokenParser::class) in bundle files

--- src/App/Plugin/TokenParser.php
+++ src/App/Plugin/TokenParser.php
@@ -51,6 +51,31 @@ class TokenParser
     }
 
     /**
+     * Peeks at a token at a certain offset from the current position
+     *
+     * @param integer $offset
+     * @return array|string
+     */
+    private function peekToken($offset)
+    {
+        $pos = $this->ptr + $offset;
+        $increment = $offset < 0 ? -1 : +1;
+        while ($pos >= 0 && $pos < $this->numTokens) {
+            $token = $this->tokens[$pos];
+            switch ($token[0]) {
+                case T_WHITESPACE:
+                case T_COMMENT:
+                case T_DOC_COMMENT:
+                    $pos += $increment;
+                    continue 2;
+                default:
+                    return $token;
+            }
+        }
+        throw new \OutOfRangeException('Tried to peek at out-of-range token at position ' . $pos);
+    }
+
+    /**
      * Advances position pointer and gets token at next position.
      *
      * Skips insignificant tokens, like whitespace and comments.
@@ -74,69 +99,89 @@ class TokenParser
 
     /**
      * Advances position pointer until a token matching a certain type is encountered.
-     * @param string[]|int[] $tokenTypes Array of single characters or token types to stop on.
+     *
+     * @param string[]|int[] $tokenTypes
+     *            Array of single characters or token types to stop on.
      * @return array|null|string Returns null if at EOF, or current token.
      */
-    private function nextUntil($tokenTypes) {
+    private function nextUntil($tokenTypes)
+    {
         do {
             $token = $this->next();
-        } while($token !== null && !in_array($token[0], $tokenTypes, true));
+        } while ($token !== null && !in_array($token[0], $tokenTypes, true));
         return $token;
     }
 
     /**
      * Asserts that the current token is of a certain type.
-     * @param int $token The asserted token type
+     *
+     * @param int $token
+     *            The asserted token type
      * @throws \LogicException When the asserted type does not match the reality.
      */
-    private function assertToken($token) {
+    private function assertToken($token)
+    {
         $currentToken = $this->getToken();
-        if(!is_array($currentToken))
-            throw new \LogicException('Expected '.token_name($token).' but got '.$currentToken);
-        if($currentToken[0] !== $token)
-            throw new \LogicException('Expected '.token_name($token).' but got '.token_name($currentToken[0]).': '.$currentToken[1]);
+        if (!is_array($currentToken))
+            throw new \LogicException('Expected ' . token_name($token) . ' but got ' . $currentToken);
+        if ($currentToken[0] !== $token)
+            throw new \LogicException('Expected ' . token_name($token) . ' but got ' . token_name($currentToken[0]) . ': ' . $currentToken[1]);
     }
 
     /**
      * Reconstructs a namespace when starting from {@link T_NAMESPACE}
+     *
      * @return string The full namespace that was parsed
      */
     private function parseNamespace()
     {
         $this->assertToken(T_NAMESPACE);
         $ns = '';
-        $token = $this->nextUntil([T_STRING, T_NS_SEPARATOR]);
+        $token = $this->nextUntil([
+            T_STRING,
+            T_NS_SEPARATOR
+        ]);
         do {
-            $ns.=$token[1];
-        } while(($token = $this->next()) && $token !== ';');
+            $ns .= $token[1];
+        } while (($token = $this->next()) && $token !== ';');
         return $ns;
     }
 
     /**
      * Constructs a class when starting from {@link T_CLASS}
+     *
      * @return null|string The class name that was parsed, or null if at EOF
      */
     private function parseClass()
     {
         $this->assertToken(T_CLASS);
+        $previousToken = $this->peekToken(-1);
+        if ($previousToken[0] === T_DOUBLE_COLON) {
+            // This was a reference to a class name (TokenParser::class)
+            return null;
+        }
         $class = '';
-        $token = $this->nextUntil([T_STRING, T_NS_SEPARATOR]);
+        $token = $this->nextUntil([
+            T_STRING,
+            T_NS_SEPARATOR
+        ]);
         do {
-            switch($token[0]) {
+            switch ($token[0]) {
                 case T_STRING:
                 case T_NS_SEPARATOR:
-                    $class.=$token[1];
+                    $class .= $token[1];
                     break;
                 default:
                     return $class;
             }
-        } while($token = $this->next());
+        } while ($token = $this->next());
 
         return null;
     }
 
     /**
      * Gets a list of all fully qualified class names declared inside the given piece of code.
+     *
      * @return string[]
      */
     public function getDefinedClasses()
